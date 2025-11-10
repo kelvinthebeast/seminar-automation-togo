f86cab6867c13ce4e6298edd4339ee6161d23864
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-11-10 07:50:58 +0700

    config ci

--- src/test/java/com/nhanthanhle/seminar/HerokuPageTest.java
+++ src/test/java/com/nhanthanhle/seminar/HerokuPageTest.java
@@ -1,67 +1,67 @@
-// package com.nhanthanhle.seminar;
-// import org.junit.jupiter.api.AfterAll;
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.Test;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.edge.EdgeDriver;
+package com.nhanthanhle.seminar;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 
-// public class HerokuPageTest {
-//     private static WebDriver myDriver;
-//     @BeforeAll
-//     public static void setUpWebDriver() {
-//         myDriver = new EdgeDriver();
-//         myDriver.get("https://the-internet.herokuapp.com/login");
-//         myDriver.manage().window().fullscreen();
+public class HerokuPageTest {
+    private static WebDriver myDriver;
+    @BeforeAll
+    public static void setUpWebDriver() {
+        myDriver = new EdgeDriver();
+        myDriver.get("https://the-internet.herokuapp.com/login");
+        myDriver.manage().window().fullscreen();
         
-//     }
+    }
 
 
 
-//     @Test
-//     void shouldLoginSuccessful() throws InterruptedException {
-//         WebElement usrnameField = myDriver.findElement(By.cssSelector("#username"));
-//         WebElement pwdField = myDriver.findElement(By.cssSelector("#password"));
-//         WebElement loginBtn = myDriver.findElement(By.cssSelector("button[type='submit']"));
+    @Test
+    void shouldLoginSuccessful() throws InterruptedException {
+        WebElement usrnameField = myDriver.findElement(By.cssSelector("#username"));
+        WebElement pwdField = myDriver.findElement(By.cssSelector("#password"));
+        WebElement loginBtn = myDriver.findElement(By.cssSelector("button[type='submit']"));
 
-//         usrnameField.sendKeys("tomsmith");
-//         pwdField.sendKeys("SuperSecretPassword!");
-//         loginBtn.click();
-
-
-//         // verify
-//         String myUrl = "https://the-internet.herokuapp.com/secure";
-//         String actualUrl = myDriver.getCurrentUrl();
+        usrnameField.sendKeys("tomsmith");
+        pwdField.sendKeys("SuperSecretPassword!");
+        loginBtn.click();
 
 
-//         Assertions.assertEquals(myUrl, actualUrl);
-//         Thread.sleep(10000);
-//     }
-
-//     @Test void shouldLoginUnSuccessful() {
-//         WebElement usrnameField = myDriver.findElement(By.cssSelector("#username"));
-//         WebElement pwdField = myDriver.findElement(By.cssSelector("#password"));
-//         WebElement loginBtn = myDriver.findElement(By.cssSelector("button[type='submit']"));
-
-//         usrnameField.sendKeys("abc");
-//         pwdField.sendKeys("abc");
-//         loginBtn.click();
+        // verify
+        String myUrl = "https://the-internet.herokuapp.com/secure";
+        String actualUrl = myDriver.getCurrentUrl();
 
 
-//         // verify
-//         String myUrl = "https://the-internet.herokuapp.com/login";
-//         String actualUrl = myDriver.getCurrentUrl();
+        Assertions.assertEquals(myUrl, actualUrl);
+        Thread.sleep(10000);
+    }
+
+    @Test void shouldLoginUnSuccessful() {
+        WebElement usrnameField = myDriver.findElement(By.cssSelector("#username"));
+        WebElement pwdField = myDriver.findElement(By.cssSelector("#password"));
+        WebElement loginBtn = myDriver.findElement(By.cssSelector("button[type='submit']"));
+
+        usrnameField.sendKeys("abc");
+        pwdField.sendKeys("abc");
+        loginBtn.click();
 
 
-//         Assertions.assertEquals(myUrl, actualUrl);
+        // verify
+        String myUrl = "https://the-internet.herokuapp.com/login";
+        String actualUrl = myDriver.getCurrentUrl();
 
-//     }
+
+        Assertions.assertEquals(myUrl, actualUrl);
+
+    }
 
 
-//     @AfterAll
-//     public static void cleanUpDriver() {
-//         myDriver.quit();
-//     }
-// }
+    @AfterAll
+    public static void cleanUpDriver() {
+        myDriver.quit();
+    }
+}
